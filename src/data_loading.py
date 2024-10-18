60c56ccb6ad703073fff8cbf4a7373da8c826669
Arnav Agrawal <arnav22097@iiitd.ac.in>
2024-10-18 12:42:32 +0530

    edited req.txt

--- src/data_loading.py
+++ src/data_loading.py
@@ -3,6 +3,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
+def load_data(filepath):
+    data = pd.read_csv(filepath)
+    return data
+
 def load_and_split_data(filepath):
     data = pd.read_csv(filepath)
     X = data.drop('quality', axis=1)
@@ -10,7 +14,3 @@ def load_and_split_data(filepath):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
-filepath = r"data\WineQT_1.csv"
-X_train, X_test, y_train, y_test = load_and_split_data(filepath)
-
-print(X_train.head())
