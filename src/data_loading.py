f2a7697fe73cf1f76a5a1d895052132da9a11b05
Arnav Agrawal <arnav22097@iiitd.ac.in>
2024-10-23 23:19:50 +0530

    final

--- src/data_loading.py
+++ src/data_loading.py
@@ -9,7 +9,8 @@ def load_data(filepath):
 
 def load_and_split_data(filepath):
     data = pd.read_csv(filepath)
-    X = data.drop('quality', axis=1)
+    data = data.drop(columns = ['Id'])
+    X = data.drop(columns = ['quality'])
     y = data['quality']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
